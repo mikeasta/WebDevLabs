e070753922073f95655c8ec8faf273179c02226c
mikeasta <thenaitrise@gmail.com>
2022-10-05 12:37:29 +0300

    Lab 2: Render issues completed. Render routes are done.

--- lab_2/routes.js
+++ lab_2/routes.js
@@ -7,37 +7,109 @@ let database = require('./database.json')
 
 // * DATABASE CONTROL SYSTEM ROUTES
 /**
- * @route GET /get_booklist
+ * @route GET /api/get_booklist
  * @desc  Loads whole booklist from database
  */
 router.get('/api/get_booklist', (req, res, next) => {
-    res.end('get_booklist')
-    next()
+    res.send(database)
 })
 
+
 /**
- * @route GET /get_book/:book_id
+ * @route GET /api/get_book/:book_id
  * @desc  Returns special book from database
  */
 router.get('/api/get_book/:book_id', (req, res, next) => {
-    res.end(`get_book/${req.params.book_id}`)
-    next()
+    // Check if book_id is NaN
+    if (isNaN(Number(book_id))) 
+        res.send(`Page not found: invalid book id`)
+    
+    // Look for book
+    const books = database.books
+    const book  = books.find(item => item.id === Number(req.params.book_id))
+    res.send(book)
 })
 
+
 /**
- * @route POST /update_book/:book_id
+ * @route PUT /api/update_book/:book_id
  * @desc  Updates special book. If book doesnt exist in DB, creates it
  */
-router.post('/api/update_book/:book_id', (req, res, next) => {
+router.put('/api/update_book/:book_id', (req, res, next) => {
+    const book_id_num = Number(book_id)
+    let book = req.body.book
 
+    // Check if book raw paremeter exists
+    if (!book) 
+        res.send(`Page not found: invalid book data`)
+
+    // Check if book_id is NaN
+    if (isNaN(book_id_num)) 
+        res.send(`Page not found: invalid book id`)
+
+    let index = -1;
+
+    // Look for book index
+    database.books.forEach((book, book_index) => {
+        if (book.id == book_id_num) {
+            index = book_index
+        }
+    })
+
+    // Check if we found smth
+    if (index === -1) {
+        res.send(`Page not found: invalid book if`)
+    } else {
+        database.books = [...database.books.slice(0, index), book, ...database.books.slice(index+1) ]
+    }
+
+    res.send(database)
 }) 
 
+
 /**
- * @route DELETE /delete_book/:book_id
+ * @route POST /api/create_new_book
+ * @desc  Creates new book
+ */
+router.post('/api/create_new_book', (req, res, next) => {
+    let book = req.body.book
+
+    // Check if book raw paremeter exists
+    if (!book) 
+        res.send(`Page not found: invalid book data`)
+
+    book.id == ++database.latest_id
+    database.books.push(book)
+    res.send(database)
+})
+
+
+/**
+ * @route DELETE /api/delete_book/:book_id
  * @desc  Deletes special book.
  */
 router.delete('/api/delete_book/:book_id', (req, res, next) => {
+    const book_id_num = Number(book_id)
 
+    // Check if book_id is NaN
+    if (isNaN(book_id_num)) 
+        res.send(`Page not found: invalid book id`)
+
+    let index = -1;
+
+    // Look for book index
+    database.books.forEach((book, book_index) => {
+        if (book.id == book_id_num) {
+            index = book_index
+        }
+    })
+
+    if (index != -1) {
+        database.books = [...database.books[0, index], ...database.books[index+1]]
+        res.send(database)
+    } else {
+        res.send(`Page not found: required book not found.`)
+    }
 })
 
 
@@ -47,8 +119,11 @@ router.delete('/api/delete_book/:book_id', (req, res, next) => {
  * @desc  Initial page (with booklist)
  */
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        value: database.books
+    })
 })
+
 
 /**
  * @route GET /add_new_book
@@ -58,17 +133,36 @@ router.get('/add_new_book', (req, res) => {
     res.render('add_book')
 })
 
+
 /**
  * @route GET /book_details/:book_id
  * @desc  Special book details page
  */
 router.get('/book_details/:book_id', (req, res) => {
-    res.render('details')
+    let index = -1
+
+    // Look for special book
+    database.books.forEach((book, idx) => {
+        if (book.id == req.params.book_id) {
+            index = idx
+        }
+    })
+
+    // Check if book exists
+    if (index === -1) {
+        res.send(`Page not found: required book not found.`)
+    }
+
+    res.render('details', {
+        value: database.books[index]
+    })
 }) 
+
 
 router.get('*', (req, res, next) => {
     res.status(404)
     res.end('Page not found')
 })
+
 
 module.exports = router
