8fe6474a770100c3f3548e1278ba958de9ee2b55
mikeasta <thenaitrise@gmail.com>
2022-10-17 12:43:24 +0300

    Lab 3: Profile page rendered

--- lab_3/routes/render.js
+++ lab_3/routes/render.js
@@ -2,6 +2,8 @@ const express = require("express")
 const router  = express.Router()
 const fs = require('fs');
 
+let database;
+
 // * PUG DRAW FUNCS
 /**
  * @route GET /
@@ -18,7 +20,7 @@ router.get('/', async (req, res) => {
  * @route GET /control_panel
  * @desc  Control page
  */
- router.get('/control_panel', async (req, res) => {
+router.get('/control_panel', async (req, res) => {
     // If ill need to load database from json after page reload
     // uncomment that row
     database = JSON.parse(fs.readFileSync('database.json'))
@@ -31,5 +33,35 @@ router.get('/', async (req, res) => {
     })
 })
 
+/**
+ * @route GET/profile/:user_id
+ * @desc  Special user page
+ */
+router.get('/profile/:user_id', async (req, res) => {
+    database = JSON.parse(fs.readFileSync('database.json'))
 
+    // Retrieve current user
+    const user_id = req.params.user_id
+    let user;
+
+    database.users.forEach(item => {
+        if (item.id == user_id) {
+            user = item
+        }
+    })
+
+    // Retrieve user's posts
+    let posts = database.posts.filter(item => {
+        return item.user_id == user_id
+    })
+
+    res.render('profile', {
+        value: {
+            access: true,
+            user: user,
+            user_friends: [],
+            user_posts: posts 
+        }
+    })
+})
 module.exports = router
