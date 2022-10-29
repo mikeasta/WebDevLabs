607a9f49a3d158ebea58e48b15b405c6b6a43a86
mikeasta <thenaitrise@gmail.com>
2022-10-29 15:47:36 +0300

    lab 3: little view changes

--- lab_3/server.js
+++ lab_3/server.js
@@ -1,31 +1,38 @@
 // Server initialization
-const fs = require("fs")
-const https = require("https")
+const fs      = require("fs")
+const https   = require("https")
 const express = require('express')
 const server  = express()
+
 
 // Init Middleware 
 // This middleware gives us oportunity to 
 // read req.body data in more comfortable json object
 server.use(express.json({ extended: false }));
 
+
 // Defining folders with static files
 server.use("/less", express.static(__dirname + '/public/less'))
 server.use("/scripts", express.static(__dirname + '/public/scripts'))
 server.use("/certificate", express.static(__dirname + '/public/certificate'))
 
+
 // Including PUG drawer engine 
 server.set('views', __dirname + "/public/views")
 server.set('view engine', 'pug')
+
 
 // API Router included
 server.use('/api/users', require('./routes/users'))
 server.use('/api/posts', require('./routes/posts'))
 server.use('/', require("./routes/render"))
 
+
 // Port defining
 const PORT = process.env.PORT || 5000
 
+
+// * HTTPS SETTINGS
 // HTTPS setup
 var privateKey  = fs.readFileSync('./public/certificate/server.key', 'utf8');
 var certificate = fs.readFileSync('./public/certificate/server.csr', 'utf8');
