d81ffab5a817affb72b674132ac0c0eaf5e66b52
mikeasta <thenaitrise@gmail.com>
2022-10-31 19:00:22 +0300

    Lab 4: initial commit

---
+++ lab_4/routes/render.js
@@ -0,0 +1,157 @@
+const express = require("express")
+const router  = express.Router()
+const fs = require('fs');
+
+let database;
+
+// * PUG DRAW FUNCS
+/**
+ * @route GET /
+ * @desc  Index page
+ */
+router.get('/', async (req, res) => {
+    res.status(200)
+    res.render('index')
+})
+
+
+/**
+ * @route GET /control_panel
+ * @desc  Control page
+ */
+router.get('/control_panel', async (req, res) => {
+    // If ill need to load database from json after page reload
+    // uncomment that row
+    database = JSON.parse(fs.readFileSync('database.json'))
+
+    let DI = require("../utils/date_interactions")
+    let SP = require("../utils/string_preprocessor")
+    let di = new DI()
+    let sp = new SP()
+
+    let users = database.users
+
+    users.forEach(user => {
+        user.birth = di.invertedToSlash(user.birth)
+        user.role = sp.getRole(user.role)
+        user.status = sp.getStatus(user.status)
+    })
+
+    // Render
+    res.status(200)
+    res.render('control_panel', {
+        value: {
+            access: true,
+            users: users
+        }
+    })
+})
+
+
+/**
+ * @route GET /profile/:user_id
+ * @desc  Special user page
+ */
+router.get('/profile/:user_id', async (req, res) => {
+    database = JSON.parse(fs.readFileSync('database.json'))
+
+    // Retrieve current user
+    const user_id = req.params.user_id
+    let user;
+
+    database.users.forEach(item => {
+        if (item.id == user_id) user = item
+    })
+
+    // If there is no user with provided id
+    if (!user) {
+        res.status(404)
+        res.render('not_found')
+        return
+    }
+
+    // Preprocess data
+    let DI = require("../utils/date_interactions")
+    let SP = require("../utils/string_preprocessor")
+    let di = new DI()
+    let sp = new SP()
+
+    user.birth  = di.invertedToSlash(user.birth)
+    user.role   = sp.getRole(user.role)
+    user.status = sp.getStatus(user.status)
+
+    // Retrieve user's posts
+    let posts = database.posts.filter(item => item.user_id == user_id)
+
+    // Retrieve user's friends
+    let friends = user.friends.map(friend_id => database.users.filter( user => user.id == friend_id)[0])
+
+    // Preprocess data
+    friends.forEach(user => {
+        user.birth  = di.invertedToSlash(user.birth)
+        user.role   = sp.getRole(user.role)
+        user.status = sp.getStatus(user.status)
+    })
+
+    posts.forEach(post => {
+        post.date = di.invertedToSlash(post.date)
+    })
+    
+
+    // Render
+    res.status(200)
+    res.render('profile', {
+        value: {
+            access: true,
+            user: user,
+            user_friends: friends,
+            user_posts: posts 
+        }
+    })
+})
+
+
+/**
+ * @route GET /edit_profile/:user_id
+ * @desc  Edit special profile page
+ */
+router.get('/edit_profile/:user_id', async (req, res) => {
+    database = JSON.parse(fs.readFileSync('database.json'))
+
+    // Retrieve current user
+    const user_id = req.params.user_id
+    let user;
+
+    database.users.forEach(item => {
+        if (item.id == user_id) user = item
+    })
+
+    // If there is no user with provided id
+    if (!user) {
+        res.status(404)
+        res.render('not_found')
+    }
+
+    // Convert date from dd/mm/yyyy to yyyy-mm-dd 
+    let DI = require("../utils/date_interactions")
+    user.birth = new DI().slashToInverted(user.birth)
+
+    // Render
+    res.status(200)
+    res.render('edit_profile', {
+        value: {
+            access: true,
+            user: user
+        }
+    })
+})
+
+
+// Not found result
+router.get('*', (req, res, next) => {
+    res.status(404)
+    res.render('not_found')
+})
+
+
+module.exports = router
---
+++ lab_4/routes/users.js
@@ -0,0 +1,197 @@
+const express = require("express")
+const router  = express.Router()
+const fs = require('fs');
+
+let database;
+
+/**
+ * @route GET /api/users/get_all_users
+ * @desc  Getting all users route
+ */
+router.get("/get_all_users", async (req, res) => {
+    database = JSON.parse(fs.readFileSync('database.json'))
+    res.status(200)
+    res.send(database.users)
+})
+
+
+/**
+ * @route GET /api/users/get_user/:user_id
+ * @desc  Getting special user by id
+ */
+router.get("/get_user/:user_id", async (req, res) => {
+    database = JSON.parse(fs.readFileSync('database.json'))
+    const user_id = req.params.user_id
+
+    // Check if user with user_id exists
+    let found;
+
+    database.users.forEach(user => {
+        if (user.id == user_id) found = user
+    })
+
+    if (!found) {
+        res.status(404)
+        res.send("User not found")
+    }
+
+    res.status(200)
+    res.send(found)
+})
+
+
+/**
+ * @route PUT /api/users/edit_user/:user_id
+ * @desc  Updates user info
+ */
+router.put("/edit_user/:user_id", async (req, res) => {
+    database = JSON.parse(fs.readFileSync('database.json'))
+
+    // Retrieve user data
+    let new_user_data = req.body.user
+    const user_id = req.params.user_id
+
+    // Check if user with user_id exists
+    let user_index = -1
+    database.users.forEach((user, index) => {
+        if (user.id == user_id) user_index = index
+    })
+
+    if (user_index == -1) {
+        res.status(404)
+        res.send("User not found")
+    }
+
+    let user    = database.users[user_index]
+    user.name   = new_user_data.name
+    user.birth  = new_user_data.birth
+    user.role   = new_user_data.role
+    user.status = new_user_data.status
+    user.email  = new_user_data.email
+    user.img    = new_user_data.img
+
+    // Save new data
+    database.users = [...database.users.slice(0, user_index), user, ...database.users.slice(user_index+1)]
+    let data = JSON.stringify(database)
+    fs.writeFileSync('database.json', data); 
+    res.status(200)
+    res.send(database.users)
+})
+
+
+/**
+ * @route PUT /api/users/ban_user/:user_id
+ * @desc  Ban special user 
+ */
+router.put("/ban_user/:user_id", async (req, res) => {
+    database = JSON.parse(fs.readFileSync('database.json'))
+
+    // Retrieve user data
+    const user_id = req.params.user_id
+
+    // Check if user with user_id exists
+    let user_index = -1
+    database.users.forEach((user, index) => {
+        if (user.id == user_id) user_index = index
+    })
+
+    if (user_index == -1) {
+        res.status(404)
+        res.send("User not found")
+    }
+
+    let user = database.users[user_index]
+    user.status = "banned"
+
+    // Save new data
+    database.users = [...database.users.slice(0, user_index), user, ...database.users.slice(user_index+1)]
+    let data = JSON.stringify(database)
+    fs.writeFileSync('database.json', data); 
+    res.status(200)
+    res.send(database.users)
+})
+
+/**
+ * @route PUT /api/users/upload_photo/:user_id
+ * @desc  Upload special user new profile avatar
+ */
+router.put("/upload_photo/:user_id", async (req, res) => {
+    database = JSON.parse(fs.readFileSync('database.json'))
+
+    // Retrieve user data
+    const user_id = req.params.user_id
+
+    // Check if user with user_id exists
+    let user_index = -1
+    database.users.forEach((user, index) => {
+        if (user.id == user_id) user_index = index
+    })
+
+    if (user_index == -1) {
+        res.status(404)
+        res.send("User not found")
+    }
+
+    let user = database.users[user_index]
+
+    user.img= req.body.img
+
+    // Save new data
+    database.users = [...database.users.slice(0, user_index), user, ...database.users.slice(user_index+1)]
+    let data = JSON.stringify(database)
+    fs.writeFileSync('database.json', data); 
+    res.status(200)
+    res.send(database.users)
+})
+
+/**
+ * @route DELETE /api/users/delete_user/:user_id
+ * @desc  Deletes special user
+ */
+router.delete("/delete_user/:user_id", async (req, res) => {
+    database = JSON.parse(fs.readFileSync('database.json'))
+
+    // Retrieve user id
+    const user_id = req.params.user_id
+
+    // Check if user with user_id exists
+    let user_index = -1
+    database.users.forEach((user, index) => {
+        if (user.id == user_id) user_index = index
+    })
+
+    if (user_index == -1) {
+        res.status(404)
+        res.send("User not found")
+    }
+
+    // Save new data
+    database.users = [...database.users.slice(0, user_index), ...database.users.slice(user_index+1)]
+
+    // Delete all user posts
+    database.posts = database.posts.filter(post => {
+        return post.user_id != user_id
+    })
+
+    // Delete this user from all friend lists
+    database.users.forEach(user => {
+        user.friends = user.friends.filter(friend_id => {
+            return friend_id != user_id
+        })
+    })
+
+    let data = JSON.stringify(database)
+    fs.writeFileSync('database.json', data); 
+    res.status(200)
+    res.send(database.users)
+})
+
+
+// Not found result
+router.get('*', (req, res, next) => {
+    res.status(404)
+    res.end('Page not found')
+})
+
+
+module.exports = router
