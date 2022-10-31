5546b52468c20c9f3f7cdc1a18ac8a3498af9efc
mikeasta <thenaitrise@gmail.com>
2022-10-31 12:13:30 +0300

    Lab 3: dist_gulp removed

--- lab_3/dist_gulp/utils/date_interactions.js
+++
@@ -1,9 +0,0 @@
-// Converts data to different forms
-class DataInteractor {
-    slashToDotDate  = date => date.split("/").join(".")
-    dotToSlashDate  = date => date.split(".").join("/")
-    invertedToSlash = date => date.split("-").reverse().join("/")
-    slashToInverted = date => date.split('/').reverse().join('-')
-}
-
-module.exports = DataInteractor
