d4d4831c4346a5a92a1354142352945eec6c79e2
mikeasta <thenaitrise@gmail.com>
2022-11-12 17:08:10 +0300

    Lab 4: initial pages setup

--- lab_4/angular-client/src/app/app.module.ts
+++ lab_4/angular-client/src/app/app.module.ts
@@ -6,8 +6,8 @@ import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 import { NewsComponent } from './news/news.component';
 import { FriendsComponent } from './friends/friends.component';
-import { CreateNewsComponent } from './create-news/create-news.component';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateNewsComponent } from './create-news/create-news.component';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent }
