4b47778e448f8425d4e42a47771fdf85670bab27
mikeasta <thenaitrise@gmail.com>
2022-12-08 17:16:43 +0300

    Lab 4: friend(-s) components initially setupped

--- lab_4/angular-client/src/app/friend/friend.component.ts
+++ lab_4/angular-client/src/app/friend/friend.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService, User } from '../services/data.service';
 
 @Component({
-  selector: 'app-friend',
-  templateUrl: './friend.component.html',
-  styleUrls: ['./friend.component.less']
+  	selector: 'friends-component',
+  	templateUrl: './friend.component.html'
 })
 export class FriendComponent implements OnInit {
 
-  constructor() { }
+  friends: User[] = [];
+  user: User = new User;
 
-  ngOnInit(): void {
-  }
+  	constructor(
+		private data: DataService
+  	) { }
 
+  	async ngOnInit(): Promise<void> {
+		this.data.friends.subscribe(friends => {
+			this.friends = friends;
+		})
+
+
+		this.fetch_friends();
+  	}
+
+	private async fetch_friends() {
+		this.data.get_friends()
+	}
 }
