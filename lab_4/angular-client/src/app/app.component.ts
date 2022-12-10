c5adfb623dcbbd41e09008de82b4e1920773cd97
mikeasta <thenaitrise@gmail.com>
2022-12-10 22:37:36 +0300

    Lab 4: access to admin panel is setupped

--- lab_4/angular-client/src/app/app.component.ts
+++ lab_4/angular-client/src/app/app.component.ts
@@ -1,20 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { DataService, User} from './services/data.service';
 
 @Component({
   	selector: 'app-root',
-  	template: `
-  <header id="common_header">
-<a routerLink="" id="common_header_index_page_link"> Главная</a>
-<a routerLink="/profile" id="common_header_profile_link">Профиль</a>
-<a routerLink="/news" id="common_header_news_link">Новости</a>
-<a routerLink="/friends" id="common_header_friends_link">Друзья</a>
-<a href="https://localhost:5000/control_panel" id="common_header_control_panel_link">Панель администрирования</a>
-</header>
-<router-outlet></router-outlet>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  `,
+  	templateUrl: './app.component.html',
   	styleUrls: ['./app.component.less']
 })
-export class AppComponent {}
+export class AppComponent {
+	constructor (
+		private http: HttpClient,
+		private router: Router,
+		private data: DataService
+	) {}
+
+	redirect_to_admin() {
+		// href="https://localhost:5000/control_panel"
+		// HTTP login request
+		this.http.get<User>(
+			`https://localhost:5000/api/users/get_user/${this.data.get_current_user_id()}`
+			).subscribe( 
+				data => {
+					data.role == "admin" ? 
+						window.location.href = "https://localhost:5000/control_panel" :
+						this.router.navigate(["/profile"]);
+				},
+				error => alert(error.error)
+			)
+	}
+}
